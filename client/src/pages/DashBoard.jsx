72199f43f23069c95c6c202ca527a40bac48d975
Himadri-16 <himadrich16@gmail.com>
2025-09-28 22:56:50 +0530

    commit 3

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
+import { useEffect } from 'react'
 
 const DashBoard = () => {
     const navigate = useNavigate()
+
+    const {companyData, setCompanyData, setCompanyToken} = useContext(AppContext)
+
+    //Function to logout for company
+    const logout = () => {
+        setCompanyToken(null)
+        localStorage.removeItem('companyToken')
+        setCompanyData(null)
+        navigate('/')
+    }
+
+    useEffect(()=>{
+        if(companyData){
+            navigate('/dashboard/manage-jobs')
+        }
+
+    },[companyData])
+
   return (
     <div className='min-h-screen'>
         {/* Navbar for recuiter */}
         <div className='shadow py-4'>
             <div className='px-5 flex justify-between items-center'>
                 <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
-                <div className='flex items-center gap-3'>
-                    <p className='max-sm:hidden'>Welcome</p>
+                {companyData && (
+                    <div className='flex items-center gap-3'>
+                    <p className='max-sm:hidden'>Welcome, {companyData.name}</p>
                     <div className='relative group'>
-                        <img className='w-8 border rounded-full' src={assets.company_icon} alt="" />
+                        <img className='w-8 border rounded-full' src={companyData.image} alt="" />
                         <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                             <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                                <li className='py-1 px-2 cursor-pointer pr-10'>LogOut</li>
+                                <li onClick={logout} className='py-1 px-2 cursor-pointer pr-10'>LogOut</li>
                             </ul>
                         </div>
                     </div>
                 </div>
+                )}
+                
             </div>
         </div>
 
@@ -42,7 +65,7 @@ const DashBoard = () => {
                 </ul>
             </div>
 
-            <div>
+            <div className='flex-1 h-full p-2 sm:p-5'>
                 <Outlet/>
             </div>
         </div>
--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,27 +1,28 @@
 import { generateInstrumentOnce } from "@sentry/node"
 import Company from "../models/Company.js"
 import bcrypt from 'bcrypt'
-import { v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import generateToken from "../utils/generateToken.js"
 import { messageInRaw } from "svix"
 import Job from "../models/Job.js"
+import JobApplication from "../models/JobApplication.js"
 
 //Register a new company
-export const registerCompany = async (req,res) =>{
+export const registerCompany = async (req, res) => {
 
-    const {name, email, password} = req.body
+    const { name, email, password } = req.body
 
     const imageFile = req.file
 
     if (!name || !email || !password || !imageFile) {
-        return res.json({success:false, message: "Missing Details"})
+        return res.json({ success: false, message: "Missing Details" })
     }
 
     try {
-        const companyExists = await Company.findOne({email})
+        const companyExists = await Company.findOne({ email })
 
-        if(companyExists){
-            return res.json({success:false, message:'Company already registered'})
+        if (companyExists) {
+            return res.json({ success: false, message: 'Company already registered' })
         }
 
         const salt = await bcrypt.genSalt(10)
@@ -48,48 +49,60 @@ export const registerCompany = async (req,res) =>{
         })
 
     } catch (error) {
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 
 }
 
 //Company login
-export const loginCompany = async (req,res) =>{
+export const loginCompany = async (req, res) => {
     const { email, password } = req.body
 
     try {
-        const company = await Company.findOne({email})
+        const company = await Company.findOne({ email })
 
-        if (bcrypt.compare(password, company.password)) {
-            
+        if (await bcrypt.compare(password, company.password)) {
+
             res.json({
-                sucess:true,
-                company:{
-                _id: company._id,
-                name: company.name,
-                email: company.email,
-                image: company.image
+                sucess: true,
+                company: {
+                    _id: company._id,
+                    name: company.name,
+                    email: company.email,
+                    image: company.image
                 },
                 token: generateToken(company._id)
             })
         }
-        else{
-            res.json({success:false, message:'Invalid email or password'})
+        else {
+            res.json({ success: false, message: 'Invalid email or password' })
         }
     } catch (error) {
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 //Get company data
-export const getCompanyData = async (req,res) =>{
+export const getCompanyData = async (req, res) => {
 
+    try {
+
+        const company = req.company
+
+        res.json({
+            success: true, company
+        })
+    } catch (error) {
+        res.json({
+            success:false, message:error.message
+        })
+    }
 }
 
 //Post a new job
-export const postJob = async (req,res) =>{
-    
-    const{ title, description, location, salary, level, category } = req.body
+export const postJob = async (req, res) => {
+
+    const { title, description, location, salary, level, category } = req.body
 
     const companyId = req.company._id
 
@@ -106,28 +119,77 @@ export const postJob = async (req,res) =>{
         })
         await newJob.save()
 
-        res.json({success:true, newJob})
+        res.json({ success: true, newJob })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//Get company job applicants
+export const getCompanyJobApplicants = async (req, res) => {
+    try {
+        const companyId = req.company._id
+
+        //Find job applications for the user and populate related data
+        const applications = await JobApplication.find({company})
+        .populate('userId','name image resume')
+        .populate('jobId','title location category level salary')
+        .exec()
+
+        return res.json({success:true, applications})
+    } catch (error) {
+        res.json({success:false, message:'error.message'})
+    }
+}
+
+//Get company posted jobs
+export const getCompanyPostedJobs = async (req, res) => {
+
+    try {
+        const companyId = req.company._id
+
+        const jobs = await Job.find({companyId})
+
+        // Adding No. of applicants info in data
+        const jobsData = await Promise.all(jobs.map(async (job) => {
+            const applicants = await JobApplication.find({jobId: job._id})
+            return {...job.toObject(),applicants:applicants.length}
+        }))
+
+        res.json({success:true, jobsData: jobs})
+    } catch (error) {
+        res.json({success:false, message: error.message})
+    }
+
+}
+
+//Change job application status
+export const ChangeJobApplicationsStatus = async (req, res) => {
+
+    try {
+          const {id,status} = req.body
+
+    await JobApplication.findOneAndUpdate({_id: id},{status})
+
+    res.json({success:true, message:'Status Changed'})
     } catch (error) {
         res.json({success:false, message: error.message})
     }
 }
 
-//Get company job applicants
-export const getCompanyJobApplicants = async (req,res) =>{
-
-}
-
-//Get company posted jobs
-export const getCompanyPostedJobs = async (req,res) =>{
-    
-}
-
-//Change job application status
-export const ChangeJobApplicationsStatus = async (req,res) =>{
-
-}
-
 //Change job visibility
-export const changeVisibility = async (req,res) =>{
-
+export const changeVisibility = async (req, res) => {
+    try {
+        
+        const{id} = req.body
+        const companyId = req.company._id
+        const job = await Job.findById(id)
+        if(companyId.toString()===job.companyId.toString()){
+            job.visible = !job.visible
+        }
+        await job.save()
+        res.json({success:true, job})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
 }
